ff3a73bd3901f4094b013e768e960a3ce371c6e3
Talia Andrews <tgandrews@wpi.edu>
2023-12-11 11:02:28 -0500

    Edits to testing input parameters.

--- tests/test_input_parameters.py
+++ tests/test_input_parameters.py
@@ -1,24 +1,28 @@
 import unittest
 from autood_parameters import get_detection_parameters
 import json
-
 from outlier_detection_methods import OutlierDetectionMethod
 
 
 def input_params_suite_setup():
     input_params_suite = unittest.TestSuite()
+    input_params_suite.addTest(ParameterParsing('get_schema_from_input'))
     return input_params_suite
 
 
 class ParameterParsing(unittest.TestCase):
-    def get_schema_from_input(self):
-        input_params = {}
-        detectors = [OutlierDetectionMethod.KNN, OutlierDetectionMethod.LOF, OutlierDetectionMethod.IsolationForest]
-        with open('\\test_files\\raw_input_params_all.json', 'r') as input_json:
-            input_params = json.load(input_json)
-        self.assertNotEqual(input_params, {})
+    input_params = {}
+    detectors = []
 
-        detection_parameters = get_detection_parameters(input_params, detectors)
+    @classmethod
+    def setUpClass(cls):
+        cls.detectors = [OutlierDetectionMethod.KNN, OutlierDetectionMethod.LOF, OutlierDetectionMethod.IsolationForest]
+        with open('\\test_files\\raw_input_params_all.json', 'r') as input_json:
+            cls.input_params = json.load(input_json)
+
+    def get_schema_from_input(self):
+        self.assertNotEqual(self.input_params, {})
+        detection_parameters = get_detection_parameters(self.input_params, self.detectors)
         print(detection_parameters)
         self.assertIsNotNone(detection_parameters)
 
