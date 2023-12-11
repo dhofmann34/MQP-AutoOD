16ca49698a3044bc03e5c7ff37c2814b7c3b4b4c
Talia Andrews <tgandrews@wpi.edu>
2023-12-11 13:29:14 -0500

    Passing unit tests for parsing an input.

--- tests/test_input_parameters.py
+++ tests/test_input_parameters.py
@@ -8,6 +8,8 @@ def input_params_suite_setup():
     input_params_suite = unittest.TestSuite()
     input_params_suite.addTest(ParameterParsing('check_result_global_range'))
     input_params_suite.addTest(ParameterParsing('check_knn'))
+    input_params_suite.addTest(ParameterParsing('check_lof'))
+    input_params_suite.addTest(ParameterParsing('check_if'))
     return input_params_suite
 
 
@@ -36,6 +38,26 @@ class ParameterParsing(unittest.TestCase):
         self.assertEqual(knn[2]['id'], 'KNN_35')
         self.assertEqual(knn[0]['params']['k'], 15)
         self.assertListEqual(knn[0]['params']['N_range'], [0.02, 0.036, 0.052, 0.068, 0.084, 0.1])
+
+    def check_lof(self):
+        lof = self.detection_parameters['local_outlier_factor']
+        self.assertEqual(len(lof), 4)
+        self.assertEqual(lof[0]['id'], 'LOF_10')
+        self.assertEqual(lof[1]['id'], 'LOF_20')
+        self.assertEqual(lof[2]['id'], 'LOF_30')
+        self.assertEqual(lof[3]['id'], 'LOF_40')
+        self.assertEqual(lof[0]['params']['k'], 10)
+        self.assertListEqual(lof[0]['params']['N_range'], [0.05, 0.07, 0.09, 0.11, 0.13, 0.15])
+
+    def check_if(self):
+        if_ = self.detection_parameters['isolation_forest']
+        self.assertEqual(len(if_), 4)
+        self.assertEqual(if_[0]['id'], 'IF_0.2')
+        self.assertEqual(if_[1]['id'], 'IF_0.3')
+        self.assertEqual(if_[2]['id'], 'IF_0.4')
+        self.assertEqual(if_[3]['id'], 'IF_1.0')
+        self.assertEqual(if_[0]['params']['max_features'], 0.2)
+        self.assertListEqual(if_[0]['params']['N_range'], [0.05, 0.07, 0.09, 0.11, 0.13, 0.15])
 
 
 # Runs the test suites
