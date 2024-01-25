418d4f3c2da559c01fc4a8f341ca1425c3f58ff1
Talia Andrews <tgandrews@wpi.edu>
2024-01-25 10:32:56 -0500

    Post request for knn runs. Has issue with session ID, looking into it.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,21 @@
 import pytest
+from werkzeug.datastructures import FileStorage
+
 from application import create_app
 
 
 @pytest.fixture()
 def app():
-    app = create_app("test_configurations.ini")
+    app = create_app("tests/test_configurations.ini")
     yield app
 
 
 @pytest.fixture()
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture()
+def cardio_file():
+    return FileStorage(stream=open("tests/test_files/cardio.csv", "rb"),
+                       filename="cardio.csv")
--- tests/test_knn.py
+++ tests/test_knn.py
@@ -1,7 +1,10 @@
 
-def test_knn_default_request(client):
-    form = {"file": "\\test_files\\cardio.csv",
-            "indexColName": "id", "labelColName": "label",
-            "outlierRangeMin": "5", "outlierRangeMax": "15", "detectionMethods": "knn"}
-    response = client.post("autood/index", data=form)
+def test_knn_default_request(client, cardio_file):
+    form = dict(
+        file=cardio_file,
+        indexColName="id", labelColName="label",
+        outlierRangeMin="5", outlierRangeMax="15",
+        detectionMethods="knn")
+    response = client.post("http://localhost:8080/autood/index", data=form,
+                           content_type='multipart/form-data')
     assert response.status_code == 200
