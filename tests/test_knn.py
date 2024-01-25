12c6092fc12bb7cb74ff4f14d3df3afb25fb87c4
Talia Andrews <tgandrews@wpi.edu>
2024-01-25 13:58:35 -0500

    Getting the tests to work with test output path

--- tests/test_knn.py
+++ tests/test_knn.py
@@ -7,6 +7,15 @@ def test_knn_default_request(client, cardio_file):
     with client.session_transaction() as session:
         session['user_id'] = '40508fa7-2b6f-4629-8187-77fe6c541333'  # Test ID in the DB
 
-    response = client.post("http://localhost:8080/autood/index",
-                            data=form, content_type='multipart/form-data')
-    assert response.status_code == 300
+    post_response = client.post("http://localhost:8080/autood/index",
+                                data=form, content_type='multipart/form-data')
+    assert post_response.status_code == 302
+
+
+def test_knn_get_results(client):
+    get_response = client.get("http://localhost:8080/autood/result")
+    assert get_response.status_code == 200
+
+def test_knn_get_results_json(client):
+    results_json = client.get("http://localhost:8080/data/")
+    assert results_json is not None
